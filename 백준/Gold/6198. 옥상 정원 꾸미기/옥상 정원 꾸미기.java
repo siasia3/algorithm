18f24826128d81f416aeff0d7e9e80b83bf0c1e1
siasia3 <131766317+siasia3@users.noreply.github.com>
2025-12-16 20:13:46 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 252 ms, Memory: 24664 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -11,30 +11,40 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
 
-        int[] building = new int[N];
-        for(int i=0;i<N;i++){
+        //int[] building = new int[N];
+        /*for(int i=0;i<N;i++){
             building[i] = Integer.parseInt(br.readLine());
-        }
-        int[] resultArray = new int[N];
-        Deque<Building> deque = new ArrayDeque<>();
+        }*/
+        //int[] resultArray = new int[N];
+        Deque<Integer> deque = new ArrayDeque<>();
         long total = 0;
 
-        for(int i=N-1;i>-1;i--) {
+        /*for(int i=N-1;i>-1;i--) {
             int height = building[i];
 
             while (!deque.isEmpty() && deque.peekLast().height < height){
-                resultArray[i] = resultArray[i] + resultArray[deque.peekLast().index] + 1;
+                //resultArray[i] = resultArray[i] + resultArray[deque.peekLast().index] + 1;
                 //total++;
                 deque.pollLast();
             }
 
             deque.addLast(new Building(i,height));
+        }*/
+
+        for(int i=0;i<N;i++){
+            int h = Integer.parseInt(br.readLine());
+            while(!deque.isEmpty() && deque.peek() <= h){
+                deque.pop();
+            }
+
+            total += deque.size();
+
+            deque.push(h);
         }
 
-
-        for(int result : resultArray){
+        /*for(int result : resultArray){
             total += result;
-        }
+        }*/
 
         bw.write(String.valueOf(total));
         bw.flush();
@@ -42,16 +52,3 @@ public class Main {
     }
 
 }
-
-class Building{
-    int index;
-    int height;
-
-    public Building(int index, int height) {
-        this.index = index;
-        this.height = height;
-    }
-}
-
-
-
